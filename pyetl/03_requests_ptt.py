8d56cbdb3c59ad8bb4ba7ac5a19bdb19a653c004
uuboyscy <aegis12321@gmail.com>
2023-06-04 10:54:01 +0800

    Update: requests

--- pyetl/03_requests_ptt.py
+++ pyetl/03_requests_ptt.py
@@ -6,3 +6,7 @@ url = "https://www.ptt.cc/bbs/joke/index.html"
 headers = {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
 }
+
+res = requests.get(url, headers=headers)
+
+print(res.text)
