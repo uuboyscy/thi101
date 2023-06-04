f67eafaba9d8b7fe24af61dd5e98a685f615ff91
uuboyscy <aegis12321@gmail.com>
2023-06-04 14:18:48 +0800

    Update: ptt

---
+++ pyetl/06_ptt_multi_pages.py
@@ -0,0 +1,27 @@
+"""This is a Python program."""
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://www.ptt.cc/bbs/joke/index%s.html"
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
+}
+
+page = 8339
+
+for _ in range(5):
+    res = requests.get(url % (page), headers=headers, timeout=30)
+
+    soup = BeautifulSoup(res.text, "html.parser")
+
+    title_tag_list = soup.select('div.title a')
+
+    # print(title_tag_list)
+    for title_tag in title_tag_list:  # <a href="/bbs/joke/M.1685841631.A.FEA.html">[趣事] Hebe告五人</a>
+        title = title_tag.text
+        article_url = "https://www.ptt.cc" + title_tag["href"]
+        print(title)
+        print(article_url)
+
+    page -= 1
---
+++ pyetl/07_ptt_multi_pages.py
@@ -0,0 +1,24 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://www.ptt.cc/bbs/joke/index.html"
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
+}
+
+for _ in range(5):
+    res = requests.get(url, headers=headers, timeout=60)
+
+    soup = BeautifulSoup(res.text, "html.parser")
+
+    title_tag_list = soup.select('div.title a')
+
+    # print(title_tag_list)
+    for title_tag in title_tag_list:  # <a href="/bbs/joke/M.1685841631.A.FEA.html">[趣事] Hebe告五人</a>
+        title = title_tag.text
+        article_url = "https://www.ptt.cc" + title_tag["href"]
+        print(title)
+        print(article_url)
+
+    url = "https://www.ptt.cc" + soup.select('a[class="btn wide"]')[1]["href"]
