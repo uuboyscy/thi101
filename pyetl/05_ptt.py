fe425a72dbfed5150b9c1cd84ed3d6a570931cca
uuboyscy <aegis12321@gmail.com>
2023-06-04 14:23:29 +0800

    Update: timeout

--- pyetl/05_ptt.py
+++ pyetl/05_ptt.py
@@ -7,7 +7,7 @@ headers = {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
 }
 
-res = requests.get(url, headers=headers)
+res = requests.get(url, headers=headers, timeout=60)
 
 soup = BeautifulSoup(res.text, "html.parser")
 
